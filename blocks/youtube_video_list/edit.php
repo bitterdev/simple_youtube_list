e70bd3d0a5792a1751558773ac3bb1dea0dc9bb4
Fabian Bitter <fabian@bitter.de>
2025-05-07 17:02:19 +0200

    Update

--- blocks/youtube_video_list/edit.php
+++ blocks/youtube_video_list/edit.php
@@ -25,7 +25,7 @@ $app = Application::getFacadeApplication();
 /** @var Form $form */
 $form = $app->make(Form::class);
 
-View::element("dashboard/help_blocktype", [], "simple_youtube_list");
+View::element("dashboard/help_blocktypes", [], "simple_youtube_list");
 ?>
 
 <?php \Concrete\Core\View\View::element("dashboard/did_you_know", [], "simple_youtube_list"); ?>
